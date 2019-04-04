d198f2ab3550f8532a6d97f9b5bc7b6f5de23c06
jan-engelmann <jan.paul.engelmann@gmail.com>
2019-04-04 11:28:36 +0200

    4 coordinates given in arbitrary order, no rotated rectangles

--- rectangles.py
+++ rectangles.py
@@ -1,9 +1,20 @@
-class Rectangle:
+# class with left, down, right, up already specified
+class Rectangle_easy:
     def __init__(self, left, down, right, up):
         self.up = up
         self.left = left
         self.right = right
         self.down = down
+
+
+# class with 4 x-y coordinates
+class Rectangle:
+    def __init__(self, c0, c1, c2, c3):
+        self.up = max(c0[1], c1[1], c2[1], c3[1])
+        self.left = min(c0[0], c1[0], c2[0], c3[0])
+        self.right = max(c0[0], c1[0], c2[0], c3[0])
+        self.down = min(c0[1], c1[1], c2[1], c3[1])
+
 
 def rects_intersect(rect_a, rect_b):
     # rectangles have to have intersecting area to qualify as intersecting. Only overlapping vertices is not enough
@@ -14,10 +25,18 @@ def rects_intersect(rect_a, rect_b):
     if rect_a.right <= rect_b.left:
         return False
     if rect_b.right <= rect_a.left:
-        return True
+        return False
+    return True
+
 
 
 if __name__ == "__main__":
-    rect1 = Rectangle(0, 0, 7, 5)
-    rect2 = Rectangle(8, 0, 9, 5)
+    rect1 = Rectangle_easy(0, 0, 7, 5)
+    rect2 = Rectangle_easy(8, 0, 9, 5)
+
+    rect_a = Rectangle((0, 0), (0, 6), (8, 6), (8, 0))
+    rect_b = Rectangle((1, 2), (1, 4), (5, 4), (5, 2))
+
     print(rects_intersect(rect1, rect2))
+
+    print(rects_intersect(rect_a, rect_b))
