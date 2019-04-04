451d1dfa6583d59cae2f067b7d4e35dd476515a6
jan-engelmann <jan.paul.engelmann@gmail.com>
2019-04-04 15:03:14 +0200

    cleaned up, straightening rectangles around origin

--- rectangles.py
+++ rectangles.py
@@ -1,45 +1,33 @@
 import math
 import copy
-# class with left, down, right, up already specified
-class Rectangle_easy:
-    def __init__(self, left, down, right, up):
-        self.up = up
-        self.left = left
-        self.right = right
-        self.down = down
 
-
-# class with 4 x-y coordinates
-class Rectangle_straight:
-    def __init__(self, c0, c1, c2, c3):
-        self.up = max(c0[1], c1[1], c2[1], c3[1])
-        self.left = min(c0[0], c1[0], c2[0], c3[0])
-        self.right = max(c0[0], c1[0], c2[0], c3[0])
-        self.down = min(c0[1], c1[1], c2[1], c3[1])
-
-
-def norm(vect):
-    return math.sqrt(vect[0]**2 + vect[1]**2)
 
 class Rectangle:
+    # initialize arbitrary rectangle with corners (x,y)
     def __init__(self, c0, c1, c2, c3):
         self.corners = [c0, c1, c2, c3]
+        # the following values will be initialized in the method straighten_around_point
         self.up = None
         self.left = None
         self.right = None
         self.down = None
-
+    def __str__(self):
+        string = ""
+        for el in self.corners:
+            string += str(el) + '\n'
+        return string
+    # this method turns the rectangle by the specific angle so that its vertices are aligned with the axes
     def straighten_around_point(self, point):
 
         y_coord = [c[1] for c in self.corners]
 
-        indices = [i[0] for i in sorted(enumerate(y_coord), key=lambda x: x[1])]
-        high = self.corners[indices[-1]]
-        low = self.corners[indices[-2]]
+        y_indices = [i[0] for i in sorted(enumerate(y_coord), key=lambda x: x[1])]
+        high = self.corners[y_indices[-1]]
+        low = self.corners[y_indices[-2]]
 
         angle = math.acos((high[0] - low[0]) / math.sqrt((high[0] - low[0])**2 + (high[1] - low[1])**2)) - math.pi/2
         for i in range(4):
-            self.corners[i] = self.rotate_point(self.corners[i], point, angle)
+            self.corners[i] = rotate_point(self.corners[i], point, angle)
 
         y_coord = [c[1] for c in self.corners]
         x_coord = [c[0] for c in self.corners]
@@ -49,52 +37,40 @@ class Rectangle:
         self.right = max(x_coord)
         self.down = min(y_coord)
 
-    def rotate_point(self, point, origin, angle):
-            x = math.cos(angle) * (point[0] - origin[0]) - math.sin(angle) * (point[1] - origin[1]) + origin[0]
-            y = math.sin(angle) * (point[0] - origin[0]) + math.cos(angle) * (point[1] - origin[1]) + origin[1]
-            return x, y
+        return angle
 
+    # this formula only works if the rectangle is straightened
     def point_in_rect(self, point):
         return point[0] < self.right and point[0] > self.left and point[1] < self.up and point[1] > self.down
 
 
+    # this is the mathematical formula for rotation of one point around a specified origin with an angle
+def rotate_point(point, origin, angle):
+        x = math.cos(angle) * (point[0] - origin[0]) - math.sin(angle) * (point[1] - origin[1]) + origin[0]
+        y = math.sin(angle) * (point[0] - origin[0]) + math.cos(angle) * (point[1] - origin[1]) + origin[1]
+        return x, y
 
-def rects_intersect_basic(rect_a, rect_b):
-    # rectangles have to have intersecting area to qualify as intersecting. Only overlapping vertices is not enough
-    if rect_a.up <= rect_b.down:
-        return False
-    if rect_b.up <= rect_a.down:
-        return False
-    if rect_a.right <= rect_b.left:
-        return False
-    if rect_b.right <= rect_a.left:
-        return False
-    return True
-
-
+# the algorithm checks for each corner of rect_b if it is inside rect_a
+# 1. turn rect_a around point so that its vertices are parallel to the axes
+# 2. check if point is inside the rectangle
 def rects_intersect(rect_a, rect_b):
     for point in rect_b.corners:
         tmp_rect = copy.deepcopy(rect_a)
-        tmp_rect.straighten_around_point(point)
+        angle = tmp_rect.straighten_around_point((0,0))
+        point = rotate_point(point, (0,0), angle)
         if tmp_rect.point_in_rect(point):
             return True
     return False
 
-# Idee, wenn Rechtecke rotiert sein können: durch 4 ecken des einen Rechtecks iterieren, das andere um diesen Punkt gerade hindrehen, checken ob punkt zwischen den Bounds ist
-
-
-
 if __name__ == "__main__":
-    rect1 = Rectangle_easy(0, 0, 7, 5)
-    rect2 = Rectangle_easy(8, 0, 9, 5)
-
     rect_a = Rectangle((0, 0), (0, 6), (8, 6), (8, 0))
     rect_b = Rectangle((1, 2), (1, 4), (5, 4), (5, 2))
-
-    print(rects_intersect_basic(rect1, rect2))
-
     print(rects_intersect(rect_a, rect_b))
 
-    rect_turn = Rectangle((-1, 0), (0, 1), (1, 0), (0, -1))
-    rect_c = Rectangle((0.9, 0.9), (2, 0.9), (0.9, 3), (2, 3))
-    print(rects_intersect(rect_turn, rect_c))
+    rect_c = Rectangle((-1, 0), (0, 1), (1, 0), (0, -1))
+    rect_d = Rectangle((0.9, 0.9), (2, 0.9), (0.9, 3), (2, 3))
+    print(rects_intersect(rect_c, rect_d))
+
+    rect_e = Rectangle((-8.937, 6.718), (10.737, 3.118), (8.937, -6.718), (-10.737, -3.118))
+    rect_f = Rectangle((60, 5), (12, 5), (60, 10), (12, 10))
+    print(rects_intersect(rect_e, rect_f))
